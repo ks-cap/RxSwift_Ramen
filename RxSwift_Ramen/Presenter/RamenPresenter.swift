af769a77ea79791c6a0382d71e2bfb1f067e2481
Ken-S-0114 <g031n071@s.iwate-pu.ac.jp>
2018-01-22 17:27:42 +0900

    ファイル構造定義

---
+++ RxSwift_Ramen/Presenter/RamenPresenter.swift
@@ -0,0 +1,15 @@
+//
+//  RamenPresenter.swift
+//  RxSwift_Ramen
+//
+//  Created by 佐藤賢 on 2018/01/22.
+//  Copyright © 2018年 佐藤賢. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxDataSources
+
+class RamenPresenter {
+
+}
