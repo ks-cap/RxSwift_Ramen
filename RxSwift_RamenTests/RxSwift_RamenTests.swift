346b8aa364497268bfdb471a559bf727388d3eba
Ken-S-0114 <g031n071@s.iwate-pu.ac.jp>
2018-01-22 17:01:21 +0900

    Initial Commit

---
+++ RxSwift_RamenTests/RxSwift_RamenTests.swift
@@ -0,0 +1,36 @@
+//
+//  RxSwift_RamenTests.swift
+//  RxSwift_RamenTests
+//
+//  Created by 佐藤賢 on 2018/01/22.
+//  Copyright © 2018年 佐藤賢. All rights reserved.
+//
+
+import XCTest
+@testable import RxSwift_Ramen
+
+class RxSwift_RamenTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
