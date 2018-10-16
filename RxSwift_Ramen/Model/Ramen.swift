d7245670a9bf2cb91f5377b9170217c5eee0b2c6
Ken-S-0114 <g031n071@s.iwate-pu.ac.jp>
2018-10-16 17:43:56 +0900

    Model再定義

--- RxSwift_Ramen/Model/Ramen.swift
+++ RxSwift_Ramen/Model/Ramen.swift
@@ -7,30 +7,28 @@
 //
 
 import UIKit
-//UITableViewやUICollectionViewのDataSourceをRxSwiftで扱う場合はこれが必要（Podfile参照）
 import RxDataSources
 
 //ラーメンデータ定義用の構造体(Model層)
 struct Ramen {
-  //取得データに関する定義
-  let name: String
-  let taste: String
-  let imageId: String
-  let image: UIImage?
-  
-  //取得データのイニシャライザ
-  init(name: String, taste: String, imageId: String) {
-    self.name = name
-    self.taste = taste
-    self.imageId = imageId
-    image = UIImage(named: imageId)
-  }
+    //取得データに関する定義
+    let name: String
+    let taste: String
+    let imageId: String
+    let image: UIImage?
+
+    //取得データのイニシャライザ
+    init(name: String, taste: String, imageId: String) {
+        self.name = name
+        self.taste = taste
+        self.imageId = imageId
+        image = UIImage(named: imageId)
+    }
 }
 
-//既存の独自型(RxDataSourcesで定義されているIdentifiableType型)を拡張する
+//既存の独自型(RxDataSourcesで定義されているIdentifiableType型)を拡張する: RxDataSourcesのプロトコル
 extension Ramen: IdentifiableType {
-  typealias Identity = String
-  var identity: Identity {
-    return imageId
-  }
+    // 一意のIDを設定
+    typealias Identity = String
+    var identity: Identity { return imageId }
 }
