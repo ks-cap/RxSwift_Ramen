a3c989c642cb46ba3e119eaa24ea9eb7e1f9e661
Ken-S-0114 <g031n071@s.iwate-pu.ac.jp>
2018-10-16 18:18:25 +0900

    ViewController再定義

--- RxSwift_Ramen/ViewController/RamenListController.swift
+++ RxSwift_Ramen/ViewController/RamenListController.swift
@@ -10,42 +10,42 @@ import RxSwift
 import RxDataSources
 
 class RamenListController: UIViewController {
-  
-  //監視対象のオブジェクトの一括解放用
-  let disposeBag = DisposeBag()
-  //Presenter層から表示するラーメンデータの取得
-  let ramensData = RamenPresenter()
-  //データソースの定義
-  let dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String, Ramen>>(
-    //データソースを元にしてセルの生成を行う
-    configureCell: {_, tableView, indexPath, ramens in
-      let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-      cell.textLabel?.text = ramens.name
-      cell.detailTextLabel?.text = ramens.taste
-      cell.imageView?.image = ramens.image
-      return cell
-    },
-    //データソースの定義を元にセクションヘッダーを生成する
-    titleForHeaderInSection: { (ds, section: Int) -> String in
-      return ds[section].model
-  }
-  )
-  
-  @IBOutlet weak var ramenTableView: UITableView!
-  override func viewDidLoad() {
-    super.viewDidLoad()
 
-    //作成したデータと表示するUITableViewをBindして表示する
-    ramensData.ramens.bind(to: ramenTableView.rx.items(dataSource: dataSource)).disposed(by: disposeBag)
-    //RxSwiftを利用してUITableViewDelegateを適用する
-    ramenTableView.rx.setDelegate(self).disposed(by: disposeBag)
-    
-  }
-  
+    // 監視対象のオブジェクトの一括解放用
+    let disposeBag = DisposeBag()
+    // Presenter層から表示するラーメンデータの取得
+    let ramensData = RamenPresenter()
+
+    // データソースの定義
+    let dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String, Ramen>>(
+        // データソースを元にしてセルの生成を行う
+        configureCell: { _, tableView, indexPath, ramens in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+            cell.textLabel?.text = ramens.name
+            cell.detailTextLabel?.text = ramens.taste
+            cell.imageView?.image = ramens.image
+            return cell
+    },
+        // データソースの定義を元にセクションヘッダーを生成する
+        titleForHeaderInSection: { (ds, section: Int) -> String in
+            return ds[section].model
+    }
+    )
+
+    @IBOutlet weak var ramenTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 作成したデータと表示するUITableViewをBindして表示する
+        ramensData.ramens.bind(to: ramenTableView.rx.items(dataSource: dataSource)).disposed(by: disposeBag)
+        // RxSwiftを利用してUITableViewDelegateを適用する
+        ramenTableView.rx.setDelegate(self).disposed(by: disposeBag)
+    }
+
 }
 
 extension RamenListController: UITableViewDelegate {
-  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    return CGFloat(65)
-  }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return CGFloat(65)
+    }
 }
